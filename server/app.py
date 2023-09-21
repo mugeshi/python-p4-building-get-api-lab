bb44aed8f195600beb79062460512a584e232480
mugeshi <nicolemugeshi@gmail.com>
2023-09-21 08:23:16 +0300

    done

--- server/app.py
+++ server/app.py
@@ -18,21 +18,55 @@ db.init_app(app)
 def index():
     return '<h1>Bakery GET API</h1>'
 
-@app.route('/bakeries')
-def bakeries():
-    return ''
+@app.route('/bakeries', methods=['GET'])
+def get_all_bakeries():
+    try:
+        bakeries = Bakery.query.all()
+        bakery_data = [{'id': bakery.id, 'name': bakery.name} for bakery in bakeries]
+        return jsonify(bakery_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-@app.route('/bakeries/<int:id>')
-def bakery_by_id(id):
-    return ''
+@app.route('/bakeries/<int:id>', methods=['GET'])
+def get_bakery_with_baked_goods(id):
+    try:
+        bakery = Bakery.query.get(id)
+        if not bakery:
+            return jsonify({'error': 'Bakery not found'}), 404
 
-@app.route('/baked_goods/by_price')
-def baked_goods_by_price():
-    return ''
+        baked_goods = BakedGood.query.filter_by(bakery_id=id).all()
+        bakery_data = {
+            'id': bakery.id,
+            'name': bakery.name,
+            'baked_goods': [{'id': bg.id, 'name': bg.name, 'price': bg.price} for bg in baked_goods]
+        }
+        return jsonify(bakery_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-@app.route('/baked_goods/most_expensive')
-def most_expensive_baked_good():
-    return ''
+@app.route('/baked_goods/by_price', methods=['GET'])
+def get_baked_goods_by_price():
+    try:
+        baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+        baked_goods_data = [{'id': bg.id, 'name': bg.name, 'price': bg.price} for bg in baked_goods]
+        return jsonify(baked_goods_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
+@app.route('/baked_goods/most_expensive', methods=['GET'])
+def get_most_expensive_baked_good():
+    try:
+        most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+        if not most_expensive_baked_good:
+            return jsonify({'error': 'No baked goods found'}), 404
+
+        baked_good_data = {
+            'id': most_expensive_baked_good.id,
+            'name': most_expensive_baked_good.name,
+            'price': most_expensive_baked_good.price
+        }
+        return jsonify(baked_good_data)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import relationship
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,9 +13,19 @@ class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
     id = db.Column(db.Integer, primary_key=True)
+     # Establish a one-to-many relationship with BakedGood
+    name = db.Column(db.String(255))
+    baked_goods = relationship('BakedGood', back_populates='bakery')
+
+
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    # Define a foreign key relationship with Bakery
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    name = db.Column(db.String(255)) 
+    price = db.Column(db.Float)
+     # Establish a many-to-one relationship with Bakery
+    bakery = relationship('Bakery', back_populates='baked_goods')
